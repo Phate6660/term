cdb074627062ac5ef7f4df1516d45f090de09632
Ash <44369810+Phate6660@users.noreply.github.com>
2019-12-02 13:33:22 +0000

    Update term.c

--- term.c
+++ term.c
@@ -3,17 +3,17 @@
 int
 main(int argc, char *argv[]) {
     GtkWidget *window, *terminal;
-
     /* Initialise GTK, the window and the terminal */
     gtk_init(&argc, &argv);
     terminal = vte_terminal_new();
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    /* various settings and customizations */
     vte_terminal_set_scroll_on_output(VTE_TERMINAL(terminal), FALSE); // dont scroll on output
     vte_terminal_set_scroll_on_keystroke(VTE_TERMINAL(terminal), TRUE); // scroll on keypress
     vte_terminal_set_mouse_autohide(VTE_TERMINAL(terminal), TRUE); // autohide mouse when typing
     vte_terminal_set_cursor_blink_mode(VTE_TERMINAL(terminal), VTE_CURSOR_BLINK_OFF); // disable cursor blink
     vte_terminal_set_allow_bold(VTE_TERMINAL(terminal), TRUE); // allow bold text
-    #define TERM_FONT "Misc Fixed 9"
+    #define TERM_FONT "Misc Fixed 9" // set the font
     PangoFontDescription *descr;
     descr = pango_font_description_from_string(TERM_FONT);
     vte_terminal_set_font(terminal, descr);
