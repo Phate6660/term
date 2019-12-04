4969e366ed34e1c9723d1876708347a64b70e593
Ash <44369810+Phate6660@users.noreply.github.com>
2019-12-04 22:56:10 +0000

    add commented out color section

--- term.c
+++ term.c
@@ -18,7 +18,35 @@ main(int argc, char *argv[]) {
     descr = pango_font_description_from_string(TERM_FONT);
     vte_terminal_set_font(terminal, descr);
     pango_font_description_free(descr);
-    char* name = NULL;
+    /* #define CLR_R(x)   (((x) & 0xff0000) >> 16)
+    #define CLR_G(x)   (((x) & 0x00ff00) >>  8)
+    #define CLR_B(x)   (((x) & 0x0000ff) >>  0)
+    #define CLR_16(x)  ((double)(x) / 0xff)
+    #define CLR_GDK(x) (const GdkRGBA){ .red = CLR_16(CLR_R(x)), \
+                                        .green = CLR_16(CLR_G(x)), \
+                                        .blue = CLR_16(CLR_B(x)), \
+                                        .alpha = 0 }
+    vte_terminal_set_colors(VTE_TERMINAL(terminal),
+        &CLR_GDK(0xffffff),
+        &(GdkRGBA){ .alpha = 0.85 },
+        (const GdkRGBA[]){
+            CLR_GDK(0x111111),
+            CLR_GDK(0xd36265),
+            CLR_GDK(0xaece91),
+            CLR_GDK(0xe7e18c),
+            CLR_GDK(0x5297cf),
+            CLR_GDK(0x963c59),
+            CLR_GDK(0x5E7175),
+            CLR_GDK(0xbebebe),
+            CLR_GDK(0x666666),
+            CLR_GDK(0xef8171),
+            CLR_GDK(0xcfefb3),
+            CLR_GDK(0xfff796),
+            CLR_GDK(0x74b8ef),
+            CLR_GDK(0xb85e7b),
+            CLR_GDK(0xA3BABF),
+            CLR_GDK(0xffffff)
+    }, 16); */
     gtk_window_set_wmclass(GTK_WINDOW (window), name ? name : "valleyTERM", "valleyTERM");
     /* Start a new shell */
     gchar **envp = g_get_environ();
